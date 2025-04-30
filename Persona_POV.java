d0d81cbc4aae032302e2d73da87a46bfc085a905
pablooOjeda <pabloojedavillasclarasa@gmail.com>
2025-04-30 18:47:45 +0200

    POV_Commit de la primera clase persona

---
+++ Persona_POV.java
@@ -0,0 +1,14 @@
+package examenRecu_entornos;
+
+public class Persona_POV {
+	private int id;
+	private int nombre;
+	private double masa;
+	private double altura;
+	private int edad;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
