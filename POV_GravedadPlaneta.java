3dc72f2aa06b0174bdf2568180b283053e83b1a8
pablooOjeda <pabloojedavillasclarasa@gmail.com>
2025-04-30 18:54:27 +0200

    POV_Commit de la primera clase persona

---
+++ POV_GravedadPlaneta.java
@@ -0,0 +1,48 @@
+package examenRecu_entornos;
+
+public class POV_GravedadPlaneta {
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getGravedadPlaneta() {
+		return GravedadPlaneta;
+	}
+
+	public void setGravedadPlaneta(double gravedadPlaneta) {
+		GravedadPlaneta = gravedadPlaneta;
+	}
+	
+	public POV_GravedadPlaneta(int id, String nombre, double gravedadPlaneta) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		GravedadPlaneta = gravedadPlaneta;
+	}
+	@Override
+	public String toString() {
+		return "POV_GravedadPlaneta [id=" + id + ", nombre=" + nombre + ", GravedadPlaneta=" + GravedadPlaneta + "]";
+	}
+	private int id;
+	private String nombre;
+	private double GravedadPlaneta;
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- Persona_POV.java
+++ Persona_POV.java
@@ -2,13 +2,56 @@ package examenRecu_entornos;
 
 public class Persona_POV {
 	private int id;
+	public Persona_POV(int id, int nombre, double masa, double altura, int edad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.masa = masa;
+		this.altura = altura;
+		this.edad = edad;
+	}
 	private int nombre;
+	@Override
+	public String toString() {
+		return "Persona_POV [id=" + id + ", nombre=" + nombre + ", masa=" + masa + ", altura=" + altura + ", edad="
+				+ edad + "]";
+	}
 	private double masa;
 	private double altura;
 	private int edad;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public double getMasa() {
+		return masa;
+	}
+	public void setMasa(double masa) {
+		this.masa = masa;
+	}
+	public int getNombre() {
+		return nombre;
+	}
+	public void setNombre(int nombre) {
+		this.nombre = nombre;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
